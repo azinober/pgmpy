cbc54c738b43658a90e3af4ba8994373a377f70e
Saba Siddique <103187876+sabasiddique1@users.noreply.github.com>
2025-12-17 18:55:51 +0500

    feat: Add HTRU2 dataset (#2484)
    - Refs: #2459 
    
    ## List of changes
    - Added HTRU2 dataset class (_htru2.py)
    - 9 variables, 17898 samples
    - Mixed data types (int64 and float64)
    - Includes expert knowledge
    - Updated __init__.py to import HTRU2
    - Added htru2 to test file ALL_DATASETS list
    
    ## Description
    What was added:
    
    - New dataset file: pgmpy/datasets/_htru2.py
    - Dataset registration in __init__.py
    - Test coverage in test_datasets.py
    - Expert knowledge from htr2.knowledge.txt
    
    Co-authored-by: Ankur Ankan <ankurankan@gmail.com>

--- pgmpy/datasets/__init__.py
+++ pgmpy/datasets/__init__.py
@@ -14,6 +14,7 @@ from ._sachs import (  # noqa: F401
     SachsMixed,
 )
 from ._boston_housing import BostonHousing  # noqa: F401
+from ._htru2 import HTRU2  # noqa: F401
 from ._yacht_hydrodynamics import YachtHydrodynamics  # noqa: F401
 from ._dry_bean import DryBean  # noqa: F401
 from ._cystic_fibrosis import CysticFibrosis  # noqa: F401
