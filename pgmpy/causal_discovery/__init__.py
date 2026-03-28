181f2a5bc7dc786ae43495268994c3048b72e53a
Aheli Poddar <ahelipoddar2003@gmail.com>
2026-03-28 05:35:57 +0530

    ENH: `ExpertInLoop` refactor (sklearn compatible)  (#2525)
    - Fixes #2573
    
    ---------
    
    Co-authored-by: Ankur Ankan <ankurankan@gmail.com>

--- pgmpy/causal_discovery/__init__.py
+++ pgmpy/causal_discovery/__init__.py
@@ -1,9 +1,11 @@
+from pgmpy.causal_discovery.ExpertInLoop import ExpertInLoop
 from pgmpy.causal_discovery.ExpertKnowledge import ExpertKnowledge
 from pgmpy.causal_discovery.GES import GES
 from pgmpy.causal_discovery.HillClimbSearch import HillClimbSearch
 from pgmpy.causal_discovery.PC import PC
 
 __all__ = [
+    "ExpertInLoop",
     "ExpertKnowledge",
     "GES",
     "HillClimbSearch",
